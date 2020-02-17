a6a3b4ac1807493cf8aab81f6a801306b66ead65
Felipe Camilo <felipe.camilo.botucatu@gmail.com>
2020-02-17 19:36:35 -0300

    Category

---
+++ src/main/java/com/felipe/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.felipe.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipe.course.entities.Category;
+import com.felipe.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
